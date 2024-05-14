4c37306697de979e7fd39be26f8bf49670a931f3
Matteo Sanfelici <sanfelicimatteo@gmail.com>
2024-05-14 14:34:10 +0200

    Exercise RomanToInt.java

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println(RomanToInt.romanToInt("III"));
+		System.out.println(RomanToInt.romanToInt("LVIII"));
+		System.out.println(RomanToInt.romanToInt("MCMXCIV"));
+	}
+
+}
